8d2516a41d3fa802fac359dc449ff06726d06eb5
rii922 <ryoga922@yahoo.co.jp>
2024-06-05 02:11:53 +0900

    [Add] add APIKeyDialog

---
+++ src/APIKeyDialog.tsx
@@ -0,0 +1,59 @@
+import * as React from 'react';
+import {
+  Dialog,
+  DialogTitle,
+  DialogContent,
+  TextField,
+  InputAdornment,
+  IconButton,
+  DialogActions,
+  Button,
+} from '@mui/material';
+import { Visibility, VisibilityOff } from '@mui/icons-material';
+
+interface Props {
+  open: boolean;
+  handleClose: () => void;
+}
+
+const APIKeyDialog: React.FC<Props> = (props) => {
+  const [showAPIKey, setShowAPIKey] = React.useState(false);
+  const handleClickShowAPIKey = () => {
+    setShowAPIKey((show) => !show);
+  };
+  const handleMouseDownShowAPIKey = (event: React.MouseEvent) => {
+    event.preventDefault();
+  };
+
+  return (
+    <Dialog open={props.open} onClose={props.handleClose} maxWidth="xs" fullWidth disableRestoreFocus>
+      <DialogTitle>Qiita APIキーを設定</DialogTitle>
+      <DialogContent>
+        <TextField
+          type={showAPIKey ? 'text' : 'password'}
+          id="apikey"
+          name="apikey"
+          autoFocus
+          fullWidth
+          variant="filled"
+          label="APIキー"
+          sx={{ my: 2 }}
+          InputProps={{
+            endAdornment: (
+              <InputAdornment position="end">
+                <IconButton onClick={handleClickShowAPIKey} onMouseDown={handleMouseDownShowAPIKey}>
+                  {showAPIKey ? <VisibilityOff /> : <Visibility />}
+                </IconButton>
+              </InputAdornment>
+            ),
+          }}
+        />
+        <DialogActions>
+          <Button onClick={props.handleClose}>閉じる</Button>
+        </DialogActions>
+      </DialogContent>
+    </Dialog>
+  );
+};
+
+export default APIKeyDialog;
--- src/SearchBar.tsx
+++ src/SearchBar.tsx
@@ -1,23 +1,35 @@
 import * as React from 'react';
 import { Paper, InputBase, Tooltip, IconButton, Divider } from '@mui/material';
 import { SearchOutlined, KeyOutlined } from '@mui/icons-material';
+import APIKeyDialog from './APIKeyDialog';
 
 const SearchBar: React.FC = () => {
+  const [open, setOpen] = React.useState(false);
+  const handleOpen = () => {
+    setOpen(true);
+  };
+  const handleClose = () => {
+    setOpen(false);
+  };
+
   return (
-    <Paper component="form" sx={{ display: 'flex', alignItems: 'center', pl: 2, pr: 1 }}>
-      <InputBase placeholder="Search..." sx={{ flex: 1, my: 1 }} />
-      <Tooltip title="検索">
-        <IconButton aria-label="search" sx={{ p: 1 }}>
-          <SearchOutlined />
-        </IconButton>
-      </Tooltip>
-      <Divider orientation="vertical" flexItem sx={{ m: 1 }} />
-      <Tooltip title="APIトークンを設定">
-        <IconButton aria-label="search" color="primary" sx={{ p: 1 }}>
-          <KeyOutlined />
-        </IconButton>
-      </Tooltip>
-    </Paper>
+    <>
+      <Paper component="form" sx={{ display: 'flex', alignItems: 'center', pl: 2, pr: 1 }}>
+        <InputBase placeholder="Search..." sx={{ flex: 1, my: 1 }} />
+        <Tooltip title="検索">
+          <IconButton aria-label="search" sx={{ p: 1 }}>
+            <SearchOutlined />
+          </IconButton>
+        </Tooltip>
+        <Divider orientation="vertical" flexItem sx={{ m: 1 }} />
+        <Tooltip title="APIキーを設定">
+          <IconButton onClick={handleOpen} aria-label="search" color="primary" sx={{ p: 1 }}>
+            <KeyOutlined />
+          </IconButton>
+        </Tooltip>
+      </Paper>
+      <APIKeyDialog open={open} handleClose={handleClose} />
+    </>
   );
 };
 
