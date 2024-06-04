f39ffab12cb98f45e21482cb18a9b608e4a1f6f4
rii922 <ryoga922@yahoo.co.jp>
2024-06-04 21:50:21 +0900

    [Update] delete comments

--- src/SearchBar.tsx
+++ src/SearchBar.tsx
@@ -1,25 +1,9 @@
 import * as React from 'react';
-// import { Grid, IconButton, TextField } from '@mui/material';
 import { Paper, InputBase, Tooltip, IconButton, Divider } from '@mui/material';
 import { SearchOutlined, KeyOutlined } from '@mui/icons-material';
 
 const SearchBar: React.FC = () => {
   return (
-    // <Grid container justifyContent="space-between" alignItems="center" spacing={2}>
-    //   <Grid item sx={{ flexGrow: 1 }}>
-    //     <TextField fullWidth label="Search..." variant="outlined" />
-    //   </Grid>
-    //   <Grid item>
-    //     <IconButton aria-label="search" color="primary">
-    //       <SearchOutlined />
-    //     </IconButton>
-    //   </Grid>
-    //   <Grid item>
-    //     <IconButton aria-label="search" color="primary">
-    //       <KeyOutlined />
-    //     </IconButton>
-    //   </Grid>
-    // </Grid>
     <Paper component="form" sx={{ display: 'flex', alignItems: 'center', pl: 2, pr: 1 }}>
       <InputBase placeholder="Search..." sx={{ flex: 1, my: 1 }} />
       <Tooltip title="検索">
