d8e2c9b1433c0a8dd373033c55cecbbfca9b3904
rii922 <ryoga922@yahoo.co.jp>
2024-06-04 15:40:44 +0900

    [Add] set eslint-config-prettier

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,11 +1,12 @@
-import globals from "globals";
-import tseslint from "typescript-eslint";
-import pluginReactConfig from "eslint-plugin-react/configs/recommended.js";
-import { fixupConfigRules } from "@eslint/compat";
-
+import globals from 'globals';
+import tseslint from 'typescript-eslint';
+import pluginReactConfig from 'eslint-plugin-react/configs/recommended.js';
+import { fixupConfigRules } from '@eslint/compat';
+import eslintConfigPrettier from 'eslint-config-prettier';
 
 export default [
-  {languageOptions: { globals: globals.browser }},
+  { languageOptions: { globals: globals.browser } },
   ...tseslint.configs.recommended,
   ...fixupConfigRules(pluginReactConfig),
+  eslintConfigPrettier,
 ];
